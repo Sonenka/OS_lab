cdbe83b343a3bb86dae23c69f49da05c98c9f9a7
Sonenka <87214826+Sonenka@users.noreply.github.com>
2024-03-21 10:58:55 +0300

    Update 1-7.sh

--- 1-7.sh
+++ 1-7.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
 
-#check the number of command-line arguments
+# Function to display help message
+display_help() {
+  echo "Usage: $0 <login_prefix> <start_number> <end_number>"
+}
+
+# Check if '-h' is provided as a command-line argument
+if [[ "$1" == "-h" ]]; then
+  display_help
+  exit 0
+fi
+
+# Check the number of command-line arguments
 if [ "$#" -ne 3 ]; then
-	echo "User: &0 <login_prefix> <start_number> <end_number>"
-	exit 1
+  echo "Error: Incorrect number of arguments."
+  display_help
+  exit 1
 fi
 
-prefix=$1
-start=$2
-end=$3
-
-#check the correctness of the ordinal numbers
-if [ $end -lt $start ]; then
-	echo "Error: end_number must be more or equal to start_number."
-	exit 1
+# Check the correctness of the ordinal numbers
+if [ $3 -lt $2 ]; then
+  echo "Error: end_number must be greater than or equal to start_number."
+  exit 1
 fi
 
-#create the user.csv file
-echo "Generating users.csv"
-rm -f users.csv
+# Function to generate users.csv
+generate_users_csv() {
+  rm -f users.csv
+  for ((i=$2; i<=$3; i++)); do
+    password=$(pwgen 12 1) # Generate a 12-character password
+    user="${1}_${i}"
+    echo "$user,$password" >> users.csv
+  done
+}
 
-for ((i=start; i<=end; i++)); do
-	password=$(pwgen 12 1) #generate a 12-character password
-	user="$prefix"_"$i"
-	echo "$user,$password" >> users.csv
-done
+if [ "$#" -eq 3 ]; then
+  # If all arguments are provided via command-line
+  prefix=$1
+  start=$2
+  end=$3
+  generate_users_csv $prefix $start $end
+  echo "user.csv has been generated."
+else
+  # If not all arguments are provided, prompt for input
+  read -p "Enter login prefix: " prefix
+  read -p "Enter start number: " start
+  read -p "Enter end number: " end
 
-echo "user.csv has generated."
+  if [ $end -lt $start ]; then
+    echo "Error: end_number must be greater than or equal to start_number."
+    exit 1
+  fi
+
+  generate_users_csv $prefix $start $end
+  echo "user.csv has been generated."
+fi
