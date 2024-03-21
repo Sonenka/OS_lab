b1203f6af68c9f3c8cb50b39412ec8128c2ad551
Sonenka <87214826+Sonenka@users.noreply.github.com>
2024-03-21 11:29:36 +0300

    Update 1-7.sh

--- 1-7.sh
+++ 1-7.sh
@@ -5,7 +5,7 @@ display_help() {
   echo "Usage: $0 <login_prefix> <start_number> <end_number>"
 }
 
-# Check if '-h' is provided as a command-line argument
+# Check if -h option is provided
 if [[ "$1" == "-h" ]]; then
   display_help
   exit 0
@@ -18,8 +18,12 @@ if [ "$#" -ne 3 ]; then
   exit 1
 fi
 
+prefix=$1
+start=$2
+end=$3
+
 # Check the correctness of the ordinal numbers
-if [ $3 -lt $2 ]; then
+if [ $end -lt $start ]; then
   echo "Error: end_number must be greater than or equal to start_number."
   exit 1
 fi
@@ -27,31 +31,12 @@ fi
 # Function to generate users.csv
 generate_users_csv() {
   rm -f users.csv
-  for ((i=$2; i<=$3; i++)); do
+  for ((i=start; i<=end; i++)); do
     password=$(pwgen 12 1) # Generate a 12-character password
-    user="${1}_${i}"
+    user="${prefix}_${i}"
     echo "$user,$password" >> users.csv
   done
 }
 
-if [ "$#" -eq 3 ]; then
-  # If all arguments are provided via command-line
-  prefix=$1
-  start=$2
-  end=$3
-  generate_users_csv $prefix $start $end
-  echo "user.csv has been generated."
-else
-  # If not all arguments are provided, prompt for input
-  read -p "Enter login prefix: " prefix
-  read -p "Enter start number: " start
-  read -p "Enter end number: " end
-
-  if [ $end -lt $start ]; then
-    echo "Error: end_number must be greater than or equal to start_number."
-    exit 1
-  fi
-
-  generate_users_csv $prefix $start $end
-  echo "user.csv has been generated."
-fi
+generate_users_csv
+echo "user.csv has been generated."
